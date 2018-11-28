f29287c680e337b6975b83572549ba26cc8b288b
Brendan <brendank196@gmail.com>
2018-11-27 23:46:44 -0500

    Replaced index-based placement and began work on FirstFitSim algorithms for allocation.

--- src/model/FirstFitSim.java
+++ src/model/FirstFitSim.java
@@ -1,7 +1,21 @@
 package model;
 
+import java.util.ArrayList;
+
 public class FirstFitSim extends MemSim {
     public FirstFitSim(int totalMemory, int osMemory) {
         super(totalMemory, osMemory);
+    }
+
+    public boolean canAllocate(ArrayList<MemProcess> pList, MemProcess mp, int totalMemory) {
+        int sum = 0;
+        int freeMemory, holeSize;
+        MemProcess temp;
+
+        for(MemProcess process : pList)
+            if(!process.getpID().equals("HOLE"))
+                sum += process.getpSize();
+
+         return false;
     }
 }
